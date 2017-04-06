4f8fc0ca9962eb8cd925e3e2ee825002e65a9b77
Gang679 <3139726292@qq.com>
2017-04-06 11:31:14 +0800

    Initial Commit

---
+++ GZPayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GZPayDemo
+//
+//  Created by xinshijie on 2017/4/6.
+//  Copyright © 2017年 Mr.quan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GZPayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GZPayDemo
+//
+//  Created by xinshijie on 2017/4/6.
+//  Copyright © 2017年 Mr.quan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
